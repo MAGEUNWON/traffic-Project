6919c369b749b0aae4b8efb2be1a5e98ae68a720
WonDuJin <windyjan1@naver.com>
2022-12-13 17:38:14 +0900

    네이버 지도 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import MapContainer from './components/MapContainer'
 import Search from './components/Search'
+import Navermap from './components/NaverMapContainer'
+
 
 declare global {
   interface Window {
@@ -10,9 +12,14 @@ declare global {
 
 const App = () =>{
   return (
+    //네이버
     <div className="root">
-      <Search/>
+      <Navermap/>
     </div>
+    ////카카오
+    // <div className="root">
+    //   <Search/>
+    // </div>
   )
 }
 
