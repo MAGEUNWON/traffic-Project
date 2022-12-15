00bddac8d5f65dda1d3d87f012bc1181f2369ebc
unknown <geunwon1947@gmail.com>
2022-12-15 16:53:01 +0900

    react로 카카오 api 지도타입 가져옴. 버튼 클릭시 지도타입 바뀌게 클릭이벤트 걸어줌

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
+import React, { useState } from "react";
 import Kakao from "./components/kakaoMap";
-import Naver from "./components/naverMap";
+// import Naver from "./components/naverMap";
 
+
+const App = () =>{
+  const [maptype,setMaptype] = useState<string>('traffic')
+
+  // const handleClick = (event:React.MouseEvent<HTMLElement>) => {
+  //   "setOverlayMapTypeID('traffic')"
+  // }
+  const handleClick = (event:React.MouseEvent<HTMLButtonElement>)=>{
+    // event.currentTarget.value
+    setMaptype(event.currentTarget.value)
+  }
+
+  return (
+    
+      <div className="root">
+        <Kakao maptype={maptype}></Kakao>  
+        <p className = "button">
+          <button value="traffic" onClick={handleClick}>도로교통정보 보기</button>
+          <button value="roadview" onClick={handleClick}>로드뷰 도로정보 보기</button>
+          <button value="terrain" onClick={handleClick}>지형정보 보기</button>
+          <button value="use_district" onClick={handleClick}>지적편집도 보기</button>
+        </p>
+
+      </div>
+    
+    
+  );
+}
 
 // const App = () =>{
 //   return (
 //     <div className="root">
-//       <Kakao></Kakao>
+//       <Naver></Naver>
 //     </div>
     
 //   );
 // }
-
-const App = () =>{
-  return (
-    <div className="root">
-      <Naver></Naver>
-    </div>
-    
-  );
-}
 
 export default App;
 
--- src/components/kakaoMap.tsx
+++ src/components/kakaoMap.tsx
@@ -10,9 +10,9 @@ declare global {
 }
 //리액트 쓸 때 전역으로 사용하기 위한 것. 컴포넌트 내부나 App.tsc에 kakao map을 선언해 줄것. 만들어 놓고 불러와서 사용하면 됨. 
 
-const Kakao = () =>{
-
+const Kakao = ({maptype}:{maptype:any}) =>{
   useEffect(()=>{
+    // console.log(maptype)
     console.log("렌더링 완료") //useEffect는 React.StrictMode가 적용된 개발환경에서는 콘솔이 두번씩 찍힘. 
     let container = document.getElementById('map') as HTMLElement;  //지도를 담을 영역의 DOM 레퍼런스
     //카카오 객체가 window 하위 객체라는 것을 정의해야 하므로 window.kakao로 변경해야 함
@@ -20,7 +20,41 @@ const Kakao = () =>{
       center: new window.kakao.maps.LatLng(36.3514622, 127.3399813),
       level: 3 //지도의 확대, 축소 정도
     };
+
     let map = new window.kakao.maps.Map(container, options);
+
+    // maptype === 'traffic'?  window.kakao.maps.MapTypeId.TRAFFIC:null
+    // maptype === 'roadview'?  window.kakao.maps.MapTypeId.ROADVIEEW:null
+    // maptype === 'use_district'?  window.kakao.maps.MapTypeId.USE_DISTRICT:null
+
+    //버튼 클릭하면 호출 (clickEvent)
+      let currentTypeId;
+      let changeMapType;
+      
+      //maptype에 따라 지도에 추가할 지도타입을 결정
+      if(maptype === 'traffic'){ //교통정보 지도타입
+        changeMapType = window.kakao.maps.MapTypeId.TRAFFIC;
+      } else if(maptype === 'roadview'){ //로드뷰 도로정보 지도타입
+        changeMapType = window.kakao.maps.MapTypeId.ROADVIEW;
+      } else if(maptype === 'terrain') { //지형정보 지도타입
+        changeMapType = window.kakao.maps.MapTypeId.TERRAIN;
+      } else if(maptype === 'use_district'){ //지적편집도 지도타입
+        changeMapType = window.kakao.maps.MapTypeId.USE_DISTRICT;
+      }
+
+      if(currentTypeId){ //이미 등록된 지도 타입이 있으면 제거
+        map.removeOverlayMapTypeId(currentTypeId);
+      }
+
+      //maptype에 해당하는 지도타입을 지도에 추가
+      map.addOverlayMapTypeId(changeMapType); 
+
+      //지도에 추가된 타입정보를 갱신
+      currentTypeId = changeMapType;
+
+    //지도에 현재 실시간 교통정보가 컬로로 표시되어 있는 라인 올림
+    // map.addOverlayMapTypeId(window.kakao.maps.MapTypeId.TRAFFIC) //MapTypeId뒤에 TRAFFIC을 SKYVIEW 등으로 바꾸면 지도 형태가 바뀜, addOverlayMapTypeId는 removeOverlayMapTypeId로 바꾸면 추가한 지도타입 제거됨
+
     //마커가 표시될 위치
     let markerPosition = new window.kakao.maps.LatLng(36.3514622, 127.3399813);
     //마커 생성
@@ -29,10 +63,16 @@ const Kakao = () =>{
     });
     //마커가 지도 위에 표시되도록 설정
     marker.setMap(map)
-  }, [])
+
+    
+
+  }, [maptype])
+
 
   return (
-    <div id = "map" style = {{width: "100vw", height: "100vh"}} />
+    <>
+    <div id = "map" style = {{width: "70vw", height: "70vh"}} /> 
+    </>
   );
 }
 
