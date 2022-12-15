42b679566595ed7dee7f63c359803637eab9c883
WonDuJin <windyjan1@naver.com>
2022-12-15 17:56:21 +0900

    python xml->json변환후 출력 성공

--- backend/daejeon.py
+++ backend/daejeon.py
@@ -5,17 +5,13 @@ import json
 class DataRoute():
 
   def daejeon() :
-    url = 'http://openapitraffic.daejeon.go.kr/traffic/rest/getTrafficInfoAll.do?serviceKey=3mzcord3q5w6kIIDIkzfjbApi4SmCWPYnUpazaf4qx4Ro1qn44qKhG69pl3aydYWaJHhEDp0LxHvx1kEyF8o%2FA%3D%3D&pageNo=1&numOfRows=500'
+    url = 'http://openapitraffic.daejeon.go.kr/traffic/rest/getTrafficInfoAll.do?serviceKey=3mzcord3q5w6kIIDIkzfjbApi4SmCWPYnUpazaf4qx4Ro1qn44qKhG69pl3aydYWaJHhEDp0LxHvx1kEyF8o%2FA%3D%3D&pageNo=1&numOfRows=50'
   # params = {"serviceKey":"3mzcord3q5w6kIIDIkzfjbApi4SmCWPYnUpazaf4qx4Ro1qn44qKhG69pl3aydYWaJHhEDp0LxHvx1kEyF8o%2FA%3D%3D","pageNo":'1',"numOfRows":'20'}
 
     res = requests.get(url)
     xmldata = res.text
-    jsonStr = json.dumps(xmltodict.parse(xmldata),ensure_ascii=False)
-    
-    
-    dict = json.loads(jsonStr)
-
-    print(dict)
+    jsonStr = json.dumps(xmltodict.parse(xmldata),ensure_ascii=False)   
+    dict = json.loads(jsonStr)    
     return dict
 
 
