dd36ea7f3f997c9667d9ffc5dd83d0db70309a12
WonDuJin <windyjan1@naver.com>
2022-12-16 18:17:30 +0900

    CCTV 영상 띄우기 시도중

--- backend/daejeon.py
+++ backend/daejeon.py
@@ -5,7 +5,9 @@ import json
 class DataRoute():
 
   def daejeon() :
-    url = 'http://openapitraffic.daejeon.go.kr/traffic/rest/getTrafficInfoAll.do?serviceKey=3mzcord3q5w6kIIDIkzfjbApi4SmCWPYnUpazaf4qx4Ro1qn44qKhG69pl3aydYWaJHhEDp0LxHvx1kEyF8o%2FA%3D%3D&pageNo=1&numOfRows=50'
+    # url = 'http://openapitraffic.daejeon.go.kr/traffic/rest/getTrafficInfoAll.do?serviceKey=3mzcord3q5w6kIIDIkzfjbApi4SmCWPYnUpazaf4qx4Ro1qn44qKhG69pl3aydYWaJHhEDp0LxHvx1kEyF8o%2FA%3D%3D&pageNo=1&numOfRows=50'
+
+    url = 'https://openapi.its.go.kr:9443/trafficInfo?apiKey=test&type=all&minX=127.247048&maxX=127.559557&minY=36.199923&maxY=36.499564&getType=json'
   # params = {"serviceKey":"3mzcord3q5w6kIIDIkzfjbApi4SmCWPYnUpazaf4qx4Ro1qn44qKhG69pl3aydYWaJHhEDp0LxHvx1kEyF8o%2FA%3D%3D","pageNo":'1',"numOfRows":'20'}
 
     res = requests.get(url)
