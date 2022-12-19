55a318296166c9bf28bc5a8d798a8eee26073dd8
WonDuJin <windyjan1@naver.com>
2022-12-19 16:03:32 +0900

    카카오 지도에 db에서 불러온 데이터에서 좌표값과 이름을 활용하여 마커를 찍고 url을 infowindow에 추가하여 마커 클릭시 cctv화면이 나오도록 설정

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -1,68 +1,139 @@
-import React,{useEffect} from "react";
+import React,{useEffect,useState} from "react";
+import axios from "axios"
 //카카오 버전
 declare global {
   interface Window {
     kakao: any;
   }
 }
-const MapContainer = ({searchPlace}:any) =>{
+
+
+const MapContainer = () =>{
+
+  //useState를 활용하여 db에서 불러온 값 저장
+  const [datas,setDatas] = useState<string>(''); 
+  axios.defaults.withCredentials = true;
   useEffect(()=>{
+    const fetchData = async ()=>{      
+      try{
+        const res = await axios.get(`http://127.0.0.1:5000/daejeon`);         
+        setDatas(res.data);        
+    }catch(e:any){
+      console.log(e)
+    }    
+  }
+  fetchData()  
+},[]);
 
-    const infowindow = new window.kakao.maps.InfoWindow({zIndex:1});
+useEffect(()=>{ 
+  // 처음 지도에 보일 화면 
+  let container = document.getElementById('map'),
+      options = {
+      center : new window.kakao.maps.LatLng(36.348991, 127.377069),
+      level : 7
+  };
+  //지도를 생성!
+  let map = new window.kakao.maps.Map(container, options);
+  
+  // 반복문을 사용하여 마커 표시
+  for(let i = 0; i <datas.length;i++){
 
-    //지도를 담을 DOM 영역
-    let container = document.getElementById('map');
-    //지도를 생성할 때 필요한 기본 옵션
-    let options = {
-      center : new window.kakao.maps.LatLng(33.450701, 126.570667),
-      level : 3
-    };
-    //지도를 생성!
-    let map = new window.kakao.maps.Map(container, options);
+    // db에서 가져온 데이터에서 이름, x좌표,y좌표 추출
+    let name = datas[i].NAME
+    let url = datas[i].URL
+    let xcode = datas[i].XCODE
+    let ycode = datas[i].YCODE
+
+    //마커 생성
+    let marker = new window.kakao.maps.Marker({
+      title : name,
+      map:map,  
+      position : new window.kakao.maps.LatLng(ycode,xcode)
+    });
+    
+    
+    
+    //infowindow 설정
+    let iwCotent = `<div style="padding:5px;">${name}</div><iframe width="330" height ="280" src = "${url}"/>`,
+    iwPosition = new window.kakao.maps.LatLng(ycode,xcode),
+    iwRemoveable = true;
+    
+    let infowindow = new window.kakao.maps.InfoWindow({      
+      position : iwPosition,
+      content : iwCotent, 
+      removable : iwRemoveable,      
+    })
+    //마커 클릭시 인포윈도우 팝업
+    window.kakao.maps.event.addListener(marker,"click",()=>{
+      infowindow.open(map,marker)
+      
+    })
+  }
+  
+
+
+
+})
+
+
+
+  // useEffect(()=>{
+
+  //   const infowindow = new window.kakao.maps.InfoWindow({zIndex:1});
+
+  //   //지도를 담을 DOM 영역
+  //   //지도를 생성할 때 필요한 기본 옵션
 
     
-    //키워드 검색이 완료됐을때 호출되는 콜백 함수
-    const placesSearchCB = (data:any,status:any,pagination:any):any =>{
-      if(status===window.kakao.maps.services.Status.OK){
+
+    
+  //   //키워드 검색이 완료됐을때 호출되는 콜백 함수
+  //   const placesSearchCB = (data:any,status:any,pagination:any):any =>{
+  //     if(status===window.kakao.maps.services.Status.OK){
         
-        //검색된 장소 위치를 기준으로 지도 범위를 재설정하기 위해 
-        // latLngBounds 객체에 좌표를 추가한다.
-        let bounds = new window.kakao.maps.LatLngBounds();
-        for (let i = 0 ; i < data.length; i++){
-          displayMaker(data[i]);
-          bounds.extend(new window.kakao.maps.LatLng(data[i].y,data[i].x));
-        }
-        //검색된 장소 위치를 기준으로 지도 위치를 재설정
-        map.setBounds(bounds)
-      }
-    }
-    //장소 검색 객체를 생성
-    let ps = new window.kakao.maps.services.Places();
-    // search.tsx에서 입력된 키워드를 검색
-    ps.keywordSearch(searchPlace,placesSearchCB);
+  //       //검색된 장소 위치를 기준으로 지도 범위를 재설정하기 위해 
+  //       // latLngBounds 객체에 좌표를 추가한다.
+  //       let bounds = new window.kakao.maps.LatLngBounds();
+  //       for (let i = 0 ; i < datas.length; i++){
+  //         displayMaker(datas[i]);
+  //         bounds.extend(new window.kakao.maps.LatLng(data[i].y,data[i].x));
+  //       }
+  //       //검색된 장소 위치를 기준으로 지도 위치를 재설정
+  //       map.setBounds(bounds)
+  //     }
+  //   }
+  //   //장소 검색 객체를 생성
+  //   let ps = new window.kakao.maps.services.Places();
+  //   // search.tsx에서 입력된 키워드를 검색
+  //   ps.keywordSearch(searchPlace,placesSearchCB);
     
 
-    // 마커 생성
-    const displayMaker = (place:any):any =>{
-      //마커를 생성하고 지도에 출력
-      let maker = new window.kakao.maps.Marker({
-        map:map,
-        position : new window.kakao.maps.LatLng(place.y,place.x)
-      });
-      // 마커에 클릭 이벤트를 추가
-      window.kakao.maps.event.addListener(maker,'click',()=>{
-        // 마커를 클릭하면 장소명이 인포윈도우에 출력된다.
-        infowindow.setContent('<iframe width="500" height ="300" src = "http://www.utic.go.kr/view/map/openDataCctvStream.jsp?key=GVjHkAH6NW3wdeJs3xypMhq52MGhEen3IP85ShkXpZ5dkKm81mXBfHzyduE0BFv&cctvid=E07002&cctvName=%25EA%25B0%2580%25EC%259E%25A5%25EA%25B5%2590%25EC%2598%25A4%25EA%25B1%25B0%25EB%25A6%25AC&kind=E&cctvip=118&cctvch=null&id=CCTV15&cctvpasswd=null&cctvport=null"/>');        
-        infowindow.open(map,maker)
-      })
-    }
+  //   // 마커 생성
+  //   const displayMaker = (place:any):any =>{
+  //     //마커를 생성하고 지도에 출력
+      // for(let i = 0; i <datas.length;i++){
+      //   let xcode = datas[i].XCODE
+      //   let ycode = datas[i].yCODE
 
-  },[searchPlace])
+      //   let maker = new window.kakao.maps.Marker({
+      //     map:map,  
+      //     position : new window.kakao.maps.LatLng(ycode.y,xcode.x)
+      //   });
+  //       // 마커에 클릭 이벤트를 추가
+  //       window.kakao.maps.event.addListener(maker,'click',()=>{
+  //         // 마커를 클릭하면 장소명이 인포윈도우에 출력된다.
+  //         infowindow.setContent('<iframe width="500" height ="300" src = "http://www.utic.go.kr/view/map/openDataCctvStream.jsp?key=GVjHkAH6NW3wdeJs3xypMhq52MGhEen3IP85ShkXpZ5dkKm81mXBfHzyduE0BFv&cctvid=E07002&cctvName=%25EA%25B0%2580%25EC%259E%25A5%25EA%25B5%2590%25EC%2598%25A4%25EA%25B1%25B0%25EB%25A6%25AC&kind=E&cctvip=118&cctvch=null&id=CCTV15&cctvpasswd=null&cctvport=null"/>');        
+  //         infowindow.open(map,maker)
+  //       })
+  //     }
+  //   }
+  // },[searchPlace])
   return(
     <div id="map" style={{width : "100vw", height : "100vh"}}>
       
     </div>
   );
+
 }
 
 export default MapContainer;
