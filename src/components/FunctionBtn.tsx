0bd83acfbf746f7315949898afe1c2e8046cd1f6
LeeSangHo <4leaf.lsh@gmail.com>
2022-12-26 18:01:39 +0900

    브랜치 이동

--- src/components/FunctionBtn.tsx
+++ src/components/FunctionBtn.tsx
@@ -3,73 +3,73 @@ import styled from "styled-components";
 import Button from "@/common/Button";
 
 const button_item = [
-  {
-    src: "asset/icon_cctv.png",
-    name: "CCTV",
-    url: "cctv"
-  },
-  {
-    src: "asset/icon_conflagration.png",
-    name: "돌발정보",
-    url: "accident"
-  },
-  {
-    src: "asset/icon_safe.png",
-    name: "보호구역",
-    url: "safezone"
-  },
-  {
-    src: "asset/icon_forecast.png",
-    name: "날씨상황",
-    url: "forecast"
-  },
-  {
-    src: "asset/icon_traffic.png",
-    name: "교통상황",
-    url: "traffic"
-  },
-  {
-    src: "asset/icon_parkinglot.png",
-    name: "주차장",
-    url: "parkinglot"
-  },
+    {
+        src: "asset/icon_cctv.png",
+        name: "CCTV",
+        url: "cctv",
+    },
+    {
+        src: "asset/icon_conflagration.png",
+        name: "돌발정보",
+        url: "accident",
+    },
+    {
+        src: "asset/icon_safe.png",
+        name: "보호구역",
+        url: "safezone",
+    },
+    {
+        src: "asset/icon_forecast.png",
+        name: "날씨상황",
+        url: "forecast",
+    },
+    {
+        src: "asset/icon_traffic.png",
+        name: "교통상황",
+        url: "traffic",
+    },
+    {
+        src: "asset/icon_parkinglot.png",
+        name: "주차장",
+        url: "parkinglot",
+    },
 ];
 
-const FunctionBtn = ({handlebuttonClick}: any) => {
-  const [activeId, setActiveId] = useState<number>();
+const FunctionBtn = ({ handlebuttonClick }: any) => {
+    const [activeId, setActiveId] = useState<number>();
 
-  const handleClick = (value: string) => {
-    handlebuttonClick(value)
-  };
+    const handleClick = (value: string) => {
+        handlebuttonClick(value);
+    };
 
-  return (
-    <ButtonDiv>
-      {button_item.map((value, index) => {
-        return (
-          <Button 
-            key={index} 
-            icon={value.src} 
-            name={value.name}
-            active={activeId === index} 
-            onClick={() => {
-              setActiveId(index);
-              handleClick(value.url);
-            }}
-          ></Button>
-        );
-      })}
-    </ButtonDiv>
-  );
+    return (
+        <ButtonDiv>
+            {button_item.map((value, index) => {
+                return (
+                    <Button
+                        key={index}
+                        icon={value.src}
+                        name={value.name}
+                        active={activeId === index}
+                        onClick={() => {
+                            setActiveId(index);
+                            handleClick(value.url);
+                        }}
+                    ></Button>
+                );
+            })}
+        </ButtonDiv>
+    );
 };
 
 const ButtonDiv = styled.div`
-  width: 218px;
-  height: 190px;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: space-around;
+    width: 218px;
+    height: 190px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-around;
 `;
 
 export default FunctionBtn;
--- src/components/FunctionMap.tsx
+++ src/components/FunctionMap.tsx
@@ -3,86 +3,91 @@ import axios from "axios";
 import FunctionSearch from "./FunctionSearch";
 
 declare global {
-  interface Window {
-    kakao: any;
-  }
+    interface Window {
+        kakao: any;
+    }
 }
 
 const FunctionMap = ({ value, searchplace }: any) => {
-  const { kakao } = window;
-  const [mapData, setMapData] = useState();
-  const [data, setData] = useState<any>([{}]);
+    const { kakao } = window;
+    const [mapData, setMapData] = useState();
+    const [data, setData] = useState<any>([{}]);
 
-  const getValue = useRef(value);
-  getValue.current = value;
+    const getValue = useRef(value);
+    getValue.current = value;
 
-  useEffect(() => {
-    const getJsonData = async () => {
-      try {
-        const response = await axios.get(`http://localhost:5000/${value}`);
-        setData(response.data);
-      } catch (err) {
-        console.log(err);
-      }
+    useEffect(() => {
+        const getJsonData = async () => {
+            try {
+                const response = await axios.get(
+                    `http://localhost:5000/${value}`
+                );
+                setData(response.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        getJsonData();
+    }, [getValue.current]);
+
+    // 지도 기본 설정 및 생성
+    useEffect(() => {
+        const mapContainer = document.getElementById("map"),
+            mapOptions = {
+                center: new kakao.maps.LatLng(
+                    36.349286539618234,
+                    127.37768781658409
+                ),
+                level: 3,
+            };
+        const map = new kakao.maps.Map(mapContainer, mapOptions);
+
+        setMapData(map);
+        FunctionSearch(searchplace, map);
+    }, [getValue.current, searchplace]);
+
+    type imgType = {
+        [key: string]: string | Array<string>;
     };
-    getJsonData();
-  }, [getValue.current]);
 
-  // 지도 기본 설정 및 생성
-  useEffect(() => {
-    const mapContainer = document.getElementById("map"),
-      mapOptions = {
-        center: new kakao.maps.LatLng(36.349286539618234, 127.37768781658409),
-        level: 3,
-      };
-    const map = new kakao.maps.Map(mapContainer, mapOptions);
-
-    setMapData(map);
-    FunctionSearch(searchplace, map);
-  }, [getValue.current, searchplace]);
-
-  type imgType = {
-    [key: string]: string | Array<string>;
-  };
-
-  const imageSrc: imgType = {
-    cctv: "/asset/cctv.png",
-    accident: [
-      "/asset/caution.png",
-      "/asset/construction.png",
-      "/asset/stop.png",
-    ],
-    safezone: "/asset/safezone.png",
-    parkinglot: "/asset/parkinglot.png",
-  };
-
-  for (let i = 0; i < data.length; i++) {
-    const imgSwitch = () => {
-      switch (data[i]["incidenteTypeCd"]) {
-        case "1":
-          return imageSrc[value][0];
-        case "2":
-          return imageSrc[value][1];
-        case "3":
-          return imageSrc[value][2];
-        default:
-          return imageSrc[value];
-      }
+    const imageSrc: imgType = {
+        cctv: "/asset/cctv.png",
+        accident: [
+            "/asset/caution.png",
+            "/asset/construction.png",
+            "/asset/stop.png",
+        ],
+        safezone: "/asset/safezone.png",
+        parkinglot: "/asset/parkinglot.png",
     };
-    const imageSize = new kakao.maps.Size(22, 22);
-    const markerImage = new kakao.maps.MarkerImage(imgSwitch(), imageSize);
 
-    const marker = new kakao.maps.Marker({
-      map: mapData,
-      position: new kakao.maps.LatLng(
-        data[i]["locationDataY"],
-        data[i]["locationDataX"]
-      ),
-      image: markerImage,
-    });
-  }
+    for (let i = 0; i < data.length; i++) {
+        const imgSwitch = () => {
+            switch (data[i]["incidenteTypeCd"]) {
+                case "1":
+                    return imageSrc[value][0];
+                case "2":
+                    return imageSrc[value][1];
+                case "3":
+                    return imageSrc[value][2];
+                default:
+                    return imageSrc[value];
+            }
+        };
+        const imageSize = new kakao.maps.Size(22, 22);
+        const markerImage = new kakao.maps.MarkerImage(imgSwitch(), imageSize);
 
-  return <div id="map" style={{ width: "80vw", height: "100vh" }} />;
+        const marker = new kakao.maps.Marker({
+            map: mapData,
+            position: new kakao.maps.LatLng(
+                data[i]["locationDataY"],
+                data[i]["locationDataX"]
+            ),
+            image: markerImage,
+        });
+    }
+
+    return <div id="map" style={{ width: "80vw", height: "100vh" }} />;
 };
 
 export default FunctionMap;
