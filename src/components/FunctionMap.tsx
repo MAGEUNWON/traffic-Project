c5830ebbdf3d41e8974e2e360024d90cc7f63d17
이상호 <kigkm@naver.com>
2022-12-27 01:45:03 +0900

    update

--- src/components/FunctionMap.tsx
+++ src/components/FunctionMap.tsx
@@ -1,108 +1,102 @@
-import { useEffect, useState, useRef } from "react";
-import axios from "axios";
-import FunctionSearch from "./FunctionSearch";
+import { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+import FunctionSearch from './FunctionSearch';
 
 declare global {
-    interface Window {
-        kakao: any;
-    }
+  interface Window {
+    kakao: any;
+  }
 }
 
 type imgType = {
-    [key: string]: string | Array<string>;
+  [key: string]: string | Array<string>;
 };
 
 const imageSrc: imgType = {
-    cctv: "/asset/cctv.png",
-    accident: [
-        "/asset/caution.png",
-        "/asset/construction.png",
-        "/asset/stop.png",
-    ],
-    safezone: "/asset/safezone.png",
-    parkinglot: "/asset/parkinglot.png",
+  cctv: '/asset/cctv.png',
+  accident: [
+    '/asset/caution.png',
+    '/asset/construction.png',
+    '/asset/stop.png',
+  ],
+  safezone: '/asset/safezone.png',
+  parkinglot: '/asset/parkinglot.png',
 };
 
 const FunctionMap = ({ value, searchplace }: any) => {
-    const { kakao } = window;
-    const [mapData, setMapData] = useState();
-    const [data, setData] = useState<any>([{}]);
-    const [markerArr, setMarkerArr] = useState<any>([]);
+  const { kakao } = window;
+  const [mapData, setMapData] = useState();
+  const [data, setData] = useState<any>([{}]);
+  const [markerArr, setMarkerArr] = useState<any>([]);
 
-    const getValue = useRef(value);
-    getValue.current = value;
+  const getValue = useRef(value);
+  getValue.current = value;
 
-    useEffect(() => {
-        markerArr.map((el: any) => {
-            el.setMap(null);
+  useEffect(() => {
+    markerArr.map((el: any) => {
+      el.setMap(null);
+    });
+
+    let arr: any = [];
+
+    for (let i = 0; i < data.length; i++) {
+      const imgSwitch = () => {
+        switch (data[i]['incidenteTypeCd']) {
+          case '1':
+            return imageSrc[value][0];
+          case '2':
+            return imageSrc[value][1];
+          case '3':
+            return imageSrc[value][2];
+          default:
+            return imageSrc[value];
+        }
+      };
+
+      const imageSize = new kakao.maps.Size(22, 22);
+      const markerImage = new kakao.maps.MarkerImage(imgSwitch(), imageSize);
+      let column1: any;
+      let column2: any;
+
+      if (data[i]['lat']) {
+        column1 = 'lat';
+        column2 = 'lon';
+      } else if (data[i]['locationDataX']) {
+        column1 = 'locationDataY';
+        column2 = 'locationDataX';
+      } else if (data[i]['XCODE']) {
+        column1 = 'YCODE';
+        column2 = 'XCODE';
+      }
+
+      let marker = new kakao.maps.Marker({
+        map: mapData,
+        position: new kakao.maps.LatLng(data[i][column1], data[i][column2]),
+        image: markerImage,
+      });
+
+      arr.push(marker);
+
+      if (data[i].CCTVID) {
+        let name = data[i].NAME;
+        let url = data[i].URL;
+        let xcode = data[i].locationDataX;
+        let ycode = data[i].locationDataY;
+
+        let iwCotent = `<div style="padding:5px;">${name}</div><iframe width="330" height ="280" style="border:none" src = "${url}"></iframe><div style="font-size:5px;background-color:#333;color:#fff">경찰청(UTIC)(LIVE)제공</div>`,
+          iwPosition = new kakao.maps.LatLng(ycode, xcode);
+
+        let infowindow = new kakao.maps.InfoWindow({
+          position: iwPosition,
+          content: iwCotent,
+          removable: true,
         });
 
-        let arr: any = [];
-
-        for (let i = 0; i < data.length; i++) {
-            const imgSwitch = () => {
-                switch (data[i]["incidenteTypeCd"]) {
-                    case "1":
-                        return imageSrc[value][0];
-                    case "2":
-                        return imageSrc[value][1];
-                    case "3":
-                        return imageSrc[value][2];
-                    default:
-                        return imageSrc[value];
-                }
-            };
-
-            const imageSize = new kakao.maps.Size(22, 22);
-            const markerImage = new kakao.maps.MarkerImage(
-                imgSwitch(),
-                imageSize
-            );
-            let column1: any;
-            let column2: any;
-
-            if (data[i]["lat"]) {
-                column1 = "lat";
-                column2 = "lon";
-            } else if (data[i]["locationDataX"]) {
-                column1 = "locationDataY";
-                column2 = "locationDataX";
-            } else if (data[i]["XCODE"]) {
-                column1 = "YCODE";
-                column2 = "XCODE";
-            }
-
-            let marker = new kakao.maps.Marker({
-                map: mapData,
-                position: new kakao.maps.LatLng(
-                    data[i][column1],
-                    data[i][column2]
-                ),
-                image: markerImage,
-            });
-
-            arr.push(marker);
-
-            if (data[i].CCTVID) {
-                let name = data[i].NAME;
-                let url = data[i].URL;
-                let xcode = data[i].locationDataX;
-                let ycode = data[i].locationDataY;
-
-                let iwCotent = `<div style="padding:5px;">${name}</div><iframe width="330" height ="280" style="border:none" src = "${url}"></iframe><div style="font-size:5px;background-color:#333;color:#fff">경찰청(UTIC)(LIVE)제공</div>`,
-                    iwPosition = new kakao.maps.LatLng(ycode, xcode);
-
-                let infowindow = new kakao.maps.InfoWindow({
-                    position: iwPosition,
-                    content: iwCotent,
-                    removable: true,
-                });
-
-                kakao.maps.event.addListener(marker, "click", () => {
-                    infowindow.open(mapData, marker);
-                });
-            } else if (data[i].lat) {
-                const content = `
+        kakao.maps.event.addListener(marker, 'click', () => {
+          infowindow.open(mapData, marker);
+        });
+      } else if (data[i].lat) {
+        const content = `
                 <div style="
                 background-color: aliceblue;
                 box-sizing: border-box;
@@ -112,139 +106,109 @@ const FunctionMap = ({ value, searchplace }: any) => {
                 </div>
               `;
 
-                // 주차장 오버레이 생성
-                const overlay = new window.kakao.maps.CustomOverlay({
-                    position: new window.kakao.maps.LatLng(
-                        data[i].lat,
-                        data[i].lon
-                    ),
-                    content: content,
-                });
+        // 주차장 오버레이 생성
+        const overlay = new window.kakao.maps.CustomOverlay({
+          position: new window.kakao.maps.LatLng(data[i].lat, data[i].lon),
+          content: content,
+        });
 
-                // 돌발정보 마커에 마우스오버하면, 해당 돌발 상황 정보 오버레이가 보인다.
-                window.kakao.maps.event.addListener(
-                    marker,
-                    "mouseover",
-                    function () {
-                        overlay.setMap(mapData);
-                    }
-                );
+        // 돌발정보 마커에 마우스오버하면, 해당 돌발 상황 정보 오버레이가 보인다.
+        window.kakao.maps.event.addListener(marker, 'mouseover', function () {
+          overlay.setMap(mapData);
+        });
 
-                // 돌발정보 마커를 마우스오버 하면, 해당 돌발 상황 정보 오버레이가 사라진다.
-                window.kakao.maps.event.addListener(
-                    marker,
-                    "mouseout",
-                    function () {
-                        overlay.setMap(null);
-                    }
-                );
-            } else if (data[i].GOV_NM) {
-                const content = `
+        // 돌발정보 마커를 마우스오버 하면, 해당 돌발 상황 정보 오버레이가 사라진다.
+        window.kakao.maps.event.addListener(marker, 'mouseout', function () {
+          overlay.setMap(null);
+        });
+      } else if (data[i].GOV_NM) {
+        const content = `
                 <div style="
                 background-color: aliceblue;
                 box-sizing: border-box;
                 padding: 10px;
                 font-size: 7px;">
-                  <div>제한속도: ${data[i]["MAX_SPD"]}</div>
-                  <div>시설명: ${data[i]["LADDR"]}</div>
+                  <div>제한속도: ${data[i]['MAX_SPD']}</div>
+                  <div>시설명: ${data[i]['LADDR']}</div>
                 </div>
               `;
-                const overlay = new window.kakao.maps.CustomOverlay({
-                    position: new window.kakao.maps.LatLng(
-                        data[i].locationDataY,
-                        data[i].locationDataX
-                    ),
-                    content: content,
-                });
+        const overlay = new window.kakao.maps.CustomOverlay({
+          position: new window.kakao.maps.LatLng(
+            data[i].locationDataY,
+            data[i].locationDataX
+          ),
+          content: content,
+        });
 
-                // 돌발정보 마커에 마우스오버하면, 해당 돌발 상황 정보 오버레이가 보인다.
-                window.kakao.maps.event.addListener(
-                    marker,
-                    "mouseover",
-                    function () {
-                        overlay.setMap(mapData);
-                    }
-                );
+        // 돌발정보 마커에 마우스오버하면, 해당 돌발 상황 정보 오버레이가 보인다.
+        window.kakao.maps.event.addListener(marker, 'mouseover', function () {
+          overlay.setMap(mapData);
+        });
 
-                // 돌발정보 마커를 마우스오버 하면, 해당 돌발 상황 정보 오버레이가 사라진다.
-                window.kakao.maps.event.addListener(
-                    marker,
-                    "mouseout",
-                    function () {
-                        overlay.setMap(null);
-                    }
-                );
-            } else if (data[i].addressJibunCd) {
-                const content = `
+        // 돌발정보 마커를 마우스오버 하면, 해당 돌발 상황 정보 오버레이가 사라진다.
+        window.kakao.maps.event.addListener(marker, 'mouseout', function () {
+          overlay.setMap(null);
+        });
+      } else if (data[i].addressJibunCd) {
+        const content = `
                 <div style="
                 background-color: aliceblue;
                 box-sizing: border-box;
                 padding: 10px;
                 font-size: 7px;">
-                  <div>돌발정보: ${data[i]["incidentTitle"]}</div>
-                  <div>도로차수: ${data[i]["lane"]}</div>
+                  <div>돌발정보: ${data[i]['incidentTitle']}</div>
+                  <div>도로차수: ${data[i]['lane']}</div>
                 </div>
               `;
-                const overlay = new window.kakao.maps.CustomOverlay({
-                    position: new window.kakao.maps.LatLng(
-                        data[i].locationDataY,
-                        data[i].locationDataX
-                    ),
-                    content: content,
-                });
+        const overlay = new window.kakao.maps.CustomOverlay({
+          position: new window.kakao.maps.LatLng(
+            data[i].locationDataY,
+            data[i].locationDataX
+          ),
+          content: content,
+        });
 
-                window.kakao.maps.event.addListener(
-                    marker,
-                    "mouseover",
-                    function () {
-                        overlay.setMap(mapData);
-                    }
-                );
+        window.kakao.maps.event.addListener(marker, 'mouseover', function () {
+          overlay.setMap(mapData);
+        });
 
-                window.kakao.maps.event.addListener(
-                    marker,
-                    "mouseout",
-                    function () {
-                        overlay.setMap(null);
-                    }
-                );
-            }
-        }
+        window.kakao.maps.event.addListener(marker, 'mouseout', function () {
+          overlay.setMap(null);
+        });
+      }
+    }
 
-        setMarkerArr(arr);
-    }, [data]);
+    setMarkerArr(arr);
+  }, [data]);
 
-    useEffect(() => {
-        const getJsonData = async () => {
-            try {
-                const response = await axios.get(
-                    `http://localhost:5000/${value}`
-                );
-                setData(response.data);
-            } catch (err) {
-                console.log(err);
-            }
-        };
-        getJsonData();
-    }, [getValue.current]);
+  useEffect(() => {
+    const getJsonData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/${value}`);
+        setData(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getJsonData();
+  }, [getValue.current]);
 
-    // 지도 기본 설정 및 생성
-    useEffect(() => {
-        const mapContainer = document.getElementById("map"),
-            mapOptions = {
-                center: new kakao.maps.LatLng(
-                    36.349286539618234,
-                    127.37768781658409
-                ),
-                level: 3,
-            };
-        const map = new kakao.maps.Map(mapContainer, mapOptions);
+  // 지도 기본 설정 및 생성
+  useEffect(() => {
+    const mapContainer = document.getElementById('map');
 
-        setMapData(map);
-        FunctionSearch(searchplace, map);
-    }, [searchplace]);
+    const map = new kakao.maps.Map(mapContainer, {
+      center: new kakao.maps.LatLng(36.349286539618234, 127.37768781658409),
+      level: 3,
+    });
 
-    return <div id="map" style={{ width: "80vw", height: "100vh" }} />;
+    setMapData(map);
+    FunctionSearch(searchplace, map);
+  }, [searchplace]);
+
+  return (
+    <div id='map' className='test' style={{ width: '80vw', height: '100vh' }} />
+  );
 };
 
 export default FunctionMap;
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,159 +1,149 @@
-import React, { useEffect, useState, useRef, forwardRef } from "react";
+import React, { useEffect, useState, useRef, forwardRef } from 'react';
 //useEffect는 리액트 컴포넌트가 렌더링 될 때마다 특정 작업을 수행하도록 설정할 수 있는 Hook. 기본적으로 렌더링되고 난 직후마다 실행.
 //useEffect에서 설정한 함수를 컴포넌트가 화면에 맨 처음 렌더링될 때만 실행하고, 업데이트될 때는 실행하지 않으려면 함수의 두 번째 파라미터로 비어 있는 배열을 넣어 주면 됨.
 //특정 값이 변경 될 때만 호출하고 싶은 경우에는 useEffect의 두 번째 파라미터로 전달되는 배열 안에 검사하고 싶은 값을 넣어주면 됨.
-import styled from "styled-components";
-import FunctionSearch from "./FunctionSearch";
+import styled from 'styled-components';
+import FunctionSearch from './FunctionSearch';
 
 declare global {
-    interface Window {
-        kakao: any;
-    }
+  interface Window {
+    kakao: any;
+  }
 }
 //리액트 쓸 때 전역으로 사용하기 위한 것. 컴포넌트 내부나 App.tsc에 kakao map을 선언해 줄것. 만들어 놓고 불러와서 사용하면 됨.
 
 interface btnSet {
-    value: string;
-    con: string;
-    [index: string]: string;
+  value: string;
+  con: string;
+  [index: string]: string;
 }
 
 const Map = forwardRef(({ searchplace, mapRef }: any) => {
-    const [mapTypes, SetMapTypes] = useState<string>("Roadmap");
+  const [mapTypes, SetMapTypes] = useState<string>('Roadmap');
 
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        SetMapTypes(e.currentTarget.value);
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    SetMapTypes(e.currentTarget.value);
+  };
+
+  const ref = useRef('');
+
+  ref.current = mapTypes;
+
+  useEffect(() => {
+    // console.log(maptype)
+    console.log('렌더링 완료'); //useEffect는 React.StrictMode가 적용된 개발환경에서는 콘솔이 두번씩 찍힘.
+
+    // axios.get(`http://127.0.0.1:5000/hazard`).then((response) => {
+    //   console.log(response.data);
+    //   setData(response.data);
+    // });
+
+    let container = document.getElementById('map') as HTMLElement; //지도를 담을 영역의 DOM 레퍼런스
+    //카카오 객체가 window 하위 객체라는 것을 정의해야 하므로 window.kakao로 변경해야 함
+    let options = {
+      center: new window.kakao.maps.LatLng(36.3492506, 127.3776511),
+      level: 3, //지도의 확대, 축소 정도
     };
 
-    const ref = useRef("");
+    let map = new window.kakao.maps.Map(container, options);
 
-    ref.current = mapTypes;
+    // let mapTypeControl = new window.kakao.maps.mapTypeControl();
+    // map.addControl(mapTypeControl, window.kakao.maps.ControlPosiiton.TOPRIGHT);
 
-    useEffect(() => {
-        // console.log(maptype)
-        console.log("렌더링 완료"); //useEffect는 React.StrictMode가 적용된 개발환경에서는 콘솔이 두번씩 찍힘.
+    // maptype === 'traffic'?  window.kakao.maps.MapTypeId.TRAFFIC:null
+    // maptype === 'roadview'?  window.kakao.maps.MapTypeId.ROADVIEEW:null
+    // maptype === 'use_district'?  window.kakao.maps.MapTypeId.USE_DISTRICT:null
 
-        // axios.get(`http://127.0.0.1:5000/hazard`).then((response) => {
-        //   console.log(response.data);
-        //   setData(response.data);
-        // });
+    //버튼 클릭하면 호출 (clickEvent)
+    let currentTypeId;
+    let changeMapType;
 
-        let container = document.getElementById("map") as HTMLElement; //지도를 담을 영역의 DOM 레퍼런스
-        //카카오 객체가 window 하위 객체라는 것을 정의해야 하므로 window.kakao로 변경해야 함
-        let options = {
-            center: new window.kakao.maps.LatLng(36.3492506, 127.3776511),
-            level: 3, //지도의 확대, 축소 정도
-        };
+    //maptype에 따라 지도에 추가할 지도타입을 결정
 
-        let map = new window.kakao.maps.Map(container, options);
-        mapRef = map;
+    if (ref.current === 'Roadmap') {
+      //로드뷰 도로정보 지도타입
+      changeMapType = window.kakao.maps.MapTypeId.ROADMAP;
+    } else if (ref.current === 'Skyview') {
+      //지형정보 지도타입
+      changeMapType = window.kakao.maps.MapTypeId.HYBRID;
+    } else if (ref.current === 'roadview') {
+      //지적편집도 지도타입
+      changeMapType = window.kakao.maps.MapTypeId.ROADVIEW;
+    }
 
-        console.log(mapRef);
+    if (currentTypeId) {
+      //이미 등록된 지도 타입이 있으면 제거
+      map.removeOverlayMapTypeId(currentTypeId);
+    }
 
-        // let mapTypeControl = new window.kakao.maps.mapTypeControl();
-        // map.addControl(mapTypeControl, window.kakao.maps.ControlPosiiton.TOPRIGHT);
+    //maptype에 해당하는 지도타입을 지도에 추가
+    map.addOverlayMapTypeId(changeMapType);
 
-        // maptype === 'traffic'?  window.kakao.maps.MapTypeId.TRAFFIC:null
-        // maptype === 'roadview'?  window.kakao.maps.MapTypeId.ROADVIEEW:null
-        // maptype === 'use_district'?  window.kakao.maps.MapTypeId.USE_DISTRICT:null
+    //지도에 추가된 타입정보를 갱신
+    currentTypeId = changeMapType;
 
-        //버튼 클릭하면 호출 (clickEvent)
-        let currentTypeId;
-        let changeMapType;
+    //지도에 현재 실시간 교통정보가 컬로로 표시되어 있는 라인 올림
+    // map.addOverlayMapTypeId(window.kakao.maps.MapTypeId.TRAFFIC) //MapTypeId뒤에 TRAFFIC을 SKYVIEW 등으로 바꾸면 지도 형태가 바뀜, addOverlayMapTypeId는 removeOverlayMapTypeId로 바꾸면 추가한 지도타입 제거됨
 
-        //maptype에 따라 지도에 추가할 지도타입을 결정
+    //마커가 표시될 위치
+    let markerPosition = new window.kakao.maps.LatLng(36.3492506, 127.3776511);
+    //마커 생성
+    let marker = new window.kakao.maps.Marker({
+      position: markerPosition,
+    });
+    //마커가 지도 위에 표시되도록 설정
+    marker.setMap(map);
 
-        if (ref.current === "Roadmap") {
-            //로드뷰 도로정보 지도타입
-            changeMapType = window.kakao.maps.MapTypeId.ROADMAP;
-        } else if (ref.current === "Skyview") {
-            //지형정보 지도타입
-            changeMapType = window.kakao.maps.MapTypeId.HYBRID;
-        } else if (ref.current === "roadview") {
-            //지적편집도 지도타입
-            changeMapType = window.kakao.maps.MapTypeId.ROADVIEW;
-        }
+    FunctionSearch(searchplace, map);
+  }, [mapTypes, searchplace]);
 
-        if (currentTypeId) {
-            //이미 등록된 지도 타입이 있으면 제거
-            map.removeOverlayMapTypeId(currentTypeId);
-        }
+  const btnSet: btnSet[] = [
+    { value: 'Roadmap', con: '지도' },
+    { value: 'Skyview', con: '스카이뷰' },
+    { value: 'roadview', con: '로드뷰' },
+  ];
 
-        //maptype에 해당하는 지도타입을 지도에 추가
-        map.addOverlayMapTypeId(changeMapType);
-
-        //지도에 추가된 타입정보를 갱신
-        currentTypeId = changeMapType;
-
-        //지도에 현재 실시간 교통정보가 컬로로 표시되어 있는 라인 올림
-        // map.addOverlayMapTypeId(window.kakao.maps.MapTypeId.TRAFFIC) //MapTypeId뒤에 TRAFFIC을 SKYVIEW 등으로 바꾸면 지도 형태가 바뀜, addOverlayMapTypeId는 removeOverlayMapTypeId로 바꾸면 추가한 지도타입 제거됨
-
-        //마커가 표시될 위치
-        let markerPosition = new window.kakao.maps.LatLng(
-            36.3492506,
-            127.3776511
-        );
-        //마커 생성
-        let marker = new window.kakao.maps.Marker({
-            position: markerPosition,
-        });
-        //마커가 지도 위에 표시되도록 설정
-        marker.setMap(map);
-
-        FunctionSearch(searchplace, map);
-    }, [mapTypes, searchplace]);
-
-    const btnSet: btnSet[] = [
-        { value: "Roadmap", con: "지도" },
-        { value: "Skyview", con: "스카이뷰" },
-        { value: "roadview", con: "로드뷰" },
-    ];
-
-    return (
-        <>
-            <div id="map" style={{ width: "80vw", height: "100vh" }} />
-            <ButtonSet>
-                {btnSet.map((value, index) => {
-                    return (
-                        <Button
-                            key={index}
-                            value={value.value}
-                            onClick={handleClick}
-                        >
-                            {value.con}
-                        </Button>
-                    );
-                })}
-            </ButtonSet>
-        </>
-    );
+  return (
+    <>
+      <div id='map' style={{ width: '80vw', height: '100vh' }} />
+      <ButtonSet>
+        {btnSet.map((value, index) => {
+          return (
+            <Button key={index} value={value.value} onClick={handleClick}>
+              {value.con}
+            </Button>
+          );
+        })}
+      </ButtonSet>
+    </>
+  );
 });
 
 const ButtonSet = styled.div`
-    display: flex;
-    width: 200px;
-    height: 30px;
-    justify-content: space-evenly;
-    position: absolute;
-    top: 1rem;
-    right: 1rem;
-    z-index: 5;
+  display: flex;
+  width: 200px;
+  height: 30px;
+  justify-content: space-evenly;
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  z-index: 5;
 `;
 
 const Button = styled.button`
-    height: 25px;
-    background-color: #1f68f6;
-    color: #fff;
-    border: none;
-    border-radius: 0.5rem;
-    font-size: 13px;
-    padding: 0 7px 0 7px;
-    box-shadow: 0 2px 3px #00000050;
-    &:hover {
-        cursor: pointer;
-        background-color: #fff;
-        color: #1f68f6;
-    }
+  height: 25px;
+  background-color: #1f68f6;
+  color: #fff;
+  border: none;
+  border-radius: 0.5rem;
+  font-size: 13px;
+  padding: 0 7px 0 7px;
+  box-shadow: 0 2px 3px #00000050;
+  &:hover {
+    cursor: pointer;
+    background-color: #fff;
+    color: #1f68f6;
+  }
 `;
 
 export default Map;
