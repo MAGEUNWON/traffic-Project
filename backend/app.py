8d30c353569262cfa7700d30bc8c2b053f641990
jeongyeonju <yeonjujeong0511@gmail.com>
2022-12-27 11:37:38 +0900

    main pull

--- backend/app.py
+++ backend/app.py
@@ -16,19 +16,22 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
 
+
 @app.route('/', methods=['GET'])
 def test():
-    
+
     return "HELLO WROLED"
 
 # CCTV
+
+
 @app.route('/cctv')
 def daejeon():
-    db_class  = DataRoute()
-    sql  = f"SELECT * From CCTV"
+    db_class = DataRoute()
+    sql = f"SELECT * From CCTV"
     row = db_class.executeAll(sql)
     return jsonify(row)
-    
+
 # -----------------------------------------
 
 
@@ -46,21 +49,25 @@ def danger_Api():
     data = ApiRoute.danger_Api()
     return data
 
-#db 테이블 내용 get요청으로 가져옴
+# db 테이블 내용 get요청으로 가져옴
+
+
 @app.route('/hazard', methods=['GET'])
 def execute():
-    db_class = DataRoute()  #얘가 변수 query인듯?
-    sql  = f"SELECT * FROM traffic.danger" #danger 테이블 내용 다 가져와라
+    db_class = DataRoute()  # 얘가 변수 query인듯?
+    sql = f"SELECT * FROM traffic.danger"  # danger 테이블 내용 다 가져와라
     # sql  = f"SELECT LOCATION_DATA FROM danger" #location 좌표만 가져옴
-    row = db_class.executeAll(sql) #executeAll은 전체 내용 다 가져오라는 명령문
+    row = db_class.executeAll(sql)  # executeAll은 전체 내용 다 가져오라는 명령문
     print(row)
     return row
+
 
 @app.route('/hazard/<polygon>', methods=['GET'])
 def Polygon(polygon):
     db_class = DataRoute()
-    sql  = f"SELECT LOCATION_DATA, DATA_DESC FROM traffic.danger WHERE LOCATION_DATA Like '%%{polygon}%%'" #location 좌표만 가져옴
-    row = db_class.executeAll(sql) #executeAll은 전체 내용 다 가져오라는 명령문
+    # location 좌표만 가져옴
+    sql = f"SELECT LOCATION_DATA, DATA_DESC FROM traffic.danger WHERE LOCATION_DATA Like '%%{polygon}%%'"
+    row = db_class.executeAll(sql)  # executeAll은 전체 내용 다 가져오라는 명령문
 
     data_stack = list()
     for i in range(len(row)):
@@ -68,25 +75,29 @@ def Polygon(polygon):
         data_stack.append(row[i])
     return jsonify(data_stack)
 
+
 @app.route('/hazard/<line>', methods=['GET'])
 def Line(line):
     db_class = DataRoute()
-    sql  = f"SELECT LOCATION_DATA FROM traffic.danger WHERE LOCATION_DATA Like '%%{line}%%'" #location 좌표만 가져옴
-    row = db_class.executeAll(sql) #executeAll은 전체 내용 다 가져오라는 명령문
+    # location 좌표만 가져옴
+    sql = f"SELECT LOCATION_DATA FROM traffic.danger WHERE LOCATION_DATA Like '%%{line}%%'"
+    row = db_class.executeAll(sql)  # executeAll은 전체 내용 다 가져오라는 명령문
     print(row)
     return jsonify(row)
+
 
 @app.route('/hazard/<point>', methods=['GET'])
 def Point(point):
     db_class = DataRoute()
-    sql  = f"SELECT LOCATION_DATA FROM traffic.danger WHERE LOCATION_DATA Like '%%{point}%%'" #location 좌표만 가져옴
-    row = db_class.executeAll(sql) #executeAll은 전체 내용 다 가져오라는 명령문
+    # location 좌표만 가져옴
+    sql = f"SELECT LOCATION_DATA FROM traffic.danger WHERE LOCATION_DATA Like '%%{point}%%'"
+    row = db_class.executeAll(sql)  # executeAll은 전체 내용 다 가져오라는 명령문
     print(row)
     return jsonify(row)
 # ----------------------------------------------------------------
 
 
-#연주
+# 연주
 
 # parkinglot
 @app.route('/parkinglot')
@@ -98,116 +109,110 @@ def parking_lot_db():
     return row
 
 # accident
+
+
 @app.route('/accident')
 def accident():
     data = ApiRoute.accident_api()
     print(data)
     return data
-#------------------------------------------------------------------
+# ------------------------------------------------------------------
 
-#화연 
+# 화연
 # safezone
+
+
 @app.route('/safezone')
 def safe_zone_db():
     db_class = DataRoute()
     sql = f"SELECT * FROM safezone_db"
     row = db_class.executeAll(sql)
-    
+
     return jsonify(row)
 
 
-#------------------------------------------------------------------
+# ------------------------------------------------------------------
 
 
-#상호 
+# 상호
 
-@app.route('/dot') # 접속하는 url
+@app.route('/dot')  # 접속하는 url
 def dot():
+    db_class = DataRoute()
+    sql = f"SELECT * FROM daejeon_node"
+    row = db_class.executeAll(sql)
+
+    return row
+
+
+@app.route('/directions', methods=['POST'])  # 접속하는 url
+def dirCall():
+    nodeData = request.json['markerArr']
+    finalData = []
+    trafficLink = []
+    trafficData = []
+
+    finalData.append(nodeData[0])
+
+    def navi(start, end):
         db_class = DataRoute()
-        sql  = f"SELECT * FROM daejeon_node"
+        sql = f"SELECT * FROM daejeon_link where F_NODE = {start['node_id']}"
         row = db_class.executeAll(sql)
 
-        return row
+        dataObj = 0
+        minData = 99999999
+        lastLink = ''
+        for i in range(len(row)):
+            db_class = DataRoute()
+            sql = f"SELECT * FROM daejeon_node where node_id = {row[i]['T_NODE']}"
+            dataRow = db_class.executeAll(sql)
 
+            Xgap = end['node_Xcode'] - dataRow[0]['node_Xcode']
+            Ygap = end['node_Ycode'] - dataRow[0]['node_Ycode']
+            line = Xgap**2+Ygap**2
 
-@app.route('/directions', methods = ['POST']) # 접속하는 url
-def dirCall():
-        nodeData = request.json['markerArr']
-        finalData = []
-        trafficLink = []
-        trafficData = []
+            if line < minData:
+                minData = line
+                dataObj = dataRow[0]
+                lastLink = row[i]['LINK_ID']
 
-        finalData.append(nodeData[0])
+        finalData.append(dataObj)
+        trafficLink.append(lastLink)
 
-        
-        def navi(start, end):
-                db_class = DataRoute() 
-                sql  = f"SELECT * FROM daejeon_link where F_NODE = {start['node_id']}"
+        if dataObj['node_id'] == end['node_id']:
+            for i in trafficLink:
+                sql = f"SELECT * FROM daejeon_traffic where linkID = {i}"
                 row = db_class.executeAll(sql)
-                
-                
-                dataObj = 0
-                minData = 99999999
-                lastLink = '';
-                for i in range(len(row)):
-                        db_class = DataRoute()
-                        sql  = f"SELECT * FROM daejeon_node where node_id = {row[i]['T_NODE']}"
-                        dataRow = db_class.executeAll(sql)
-                        
-
-                        Xgap = end['node_Xcode'] - dataRow[0]['node_Xcode']
-                        Ygap = end['node_Ycode'] - dataRow[0]['node_Ycode']
-                        line = Xgap**2+Ygap**2
-
-                        if line < minData:
-                                minData = line
-                                dataObj = dataRow[0]
-                                lastLink = row[i]['LINK_ID']
-
-                finalData.append(dataObj)
-                trafficLink.append(lastLink)
-
-
-
-
-                if dataObj['node_id'] == end['node_id']:
-                        for i in trafficLink:
-                                sql  = f"SELECT * FROM daejeon_traffic where linkID = {i}"
-                                row = db_class.executeAll(sql)
-                                if len(row) != 0:
-                                        trafficData.append(row[0])
-                                else:
-                                        trafficData.append({
-                                                            "congestion": "정보없음",
-                                                            "endNodeID": "정보없음",
-                                                            "endNodeName": "정보없음",
-                                                            "linkCount": "정보없음",
-                                                            "linkID": "정보없음",
-                                                            "linkLength": "정보없음",
-                                                            "linkSqc": "정보없음",
-                                                            "roadName": "정보없음",
-                                                            "speed":"정보없음",
-                                                            "startNodeId": "정보없음",
-                                                            "startNodeName": "정보없음",
-                                                            "travelT": "정보없음",
-                                                            "udType": "정보없음"
-                                                            })
-                                
-
-                        print('end')
-                
+                if len(row) != 0:
+                    trafficData.append(row[0])
                 else:
-                        navi(dataObj,end)
+                    trafficData.append({
+                        "congestion": "정보없음",
+                        "endNodeID": "정보없음",
+                        "endNodeName": "정보없음",
+                        "linkCount": "정보없음",
+                        "linkID": "정보없음",
+                        "linkLength": "정보없음",
+                        "linkSqc": "정보없음",
+                        "roadName": "정보없음",
+                        "speed": "정보없음",
+                        "startNodeId": "정보없음",
+                        "startNodeName": "정보없음",
+                        "travelT": "정보없음",
+                        "udType": "정보없음"
+                    })
+
+            print('end')
+
+        else:
+            navi(dataObj, end)
+
+    navi(nodeData[0], nodeData[1])
+
+    return {"finalData": finalData, "trafficData": trafficData}
 
 
-        navi(nodeData[0],nodeData[1])
-
-        return {"finalData":finalData, "trafficData":trafficData}
-
-
-#------------------------------------------------------------------
-
-
+# ------------------------------------------------------------------
 
 
 # # CCTV
@@ -218,14 +223,14 @@ def dirCall():
 #     row = db_class.executeAll(sql)
 
 #     return jsonify(row)
-    
+
 # # safezone
 # @app.route('/safezone')
 # def safe_zone_db():
 #     db_class = DataRoute()
 #     sql = f"SELECT * FROM safezone_db"
 #     row = db_class.executeAll(sql)
-    
+
 #     return jsonify(row)
 
 # # parkinglot
@@ -244,88 +249,80 @@ def dirCall():
 #     return data
 
 #
-@app.route('/dot') # 접속하는 url
-def dot():
-        db_class = DataRoute()
-        sql  = f"SELECT * FROM daejeon_node"
-        row = db_class.executeAll(sql)
+# @app.route('/dot') # 접속하는 url
+# def dot():
+#         db_class = DataRoute()
+#         sql  = f"SELECT * FROM daejeon_node"
+#         row = db_class.executeAll(sql)
 
-        return row
-
-
-@app.route('/directions', methods = ['POST']) # 접속하는 url
-def dirCall():
-        nodeData = request.json['markerArr']
-        finalData = []
-        trafficLink = []
-        trafficData = []
-
-        finalData.append(nodeData[0])
-
-        
-        def navi(start, end):
-                db_class = DataRoute() 
-                sql  = f"SELECT * FROM daejeon_link where F_NODE = {start['node_id']}"
-                row = db_class.executeAll(sql)
-                
-                
-                dataObj = 0
-                minData = 99999999
-                lastLink = '';
-                for i in range(len(row)):
-                        db_class = DataRoute()
-                        sql  = f"SELECT * FROM daejeon_node where node_id = {row[i]['T_NODE']}"
-                        dataRow = db_class.executeAll(sql)
-                        
-
-                        Xgap = end['node_Xcode'] - dataRow[0]['node_Xcode']
-                        Ygap = end['node_Ycode'] - dataRow[0]['node_Ycode']
-                        line = Xgap**2+Ygap**2
-
-                        if line < minData:
-                                minData = line
-                                dataObj = dataRow[0]
-                                lastLink = row[i]['LINK_ID']
-
-                finalData.append(dataObj)
-                trafficLink.append(lastLink)
+#         return row
 
 
+# @app.route('/directions', methods=['POST'])  # 접속하는 url
+# def dirCall():
+#     nodeData = request.json['markerArr']
+#     finalData = []
+#     trafficLink = []
+#     trafficData = []
 
+#     finalData.append(nodeData[0])
 
-                if dataObj['node_id'] == end['node_id']:
-                        for i in trafficLink:
-                                sql  = f"SELECT * FROM daejeon_traffic where linkID = {i}"
-                                row = db_class.executeAll(sql)
-                                if len(row) != 0:
-                                        trafficData.append(row[0])
-                                else:
-                                        trafficData.append({
-                                                            "congestion": "정보없음",
-                                                            "endNodeID": "정보없음",
-                                                            "endNodeName": "정보없음",
-                                                            "linkCount": "정보없음",
-                                                            "linkID": "정보없음",
-                                                            "linkLength": "정보없음",
-                                                            "linkSqc": "정보없음",
-                                                            "roadName": "정보없음",
-                                                            "speed":"정보없음",
-                                                            "startNodeId": "정보없음",
-                                                            "startNodeName": "정보없음",
-                                                            "travelT": "정보없음",
-                                                            "udType": "정보없음"
-                                                            })
-                                
+#     def navi(start, end):
+#         db_class = DataRoute()
+#         sql = f"SELECT * FROM daejeon_link where F_NODE = {start['node_id']}"
+#         row = db_class.executeAll(sql)
 
-                        print('end')
-                
-                else:
-                        navi(dataObj,end)
+#         dataObj = 0
+#         minData = 99999999
+#         lastLink = ''
+#         for i in range(len(row)):
+#             db_class = DataRoute()
+#             sql = f"SELECT * FROM daejeon_node where node_id = {row[i]['T_NODE']}"
+#             dataRow = db_class.executeAll(sql)
 
+#             Xgap = end['node_Xcode'] - dataRow[0]['node_Xcode']
+#             Ygap = end['node_Ycode'] - dataRow[0]['node_Ycode']
+#             line = Xgap**2+Ygap**2
 
-        navi(nodeData[0],nodeData[1])
+#             if line < minData:
+#                 minData = line
+#                 dataObj = dataRow[0]
+#                 lastLink = row[i]['LINK_ID']
 
-        return {"finalData":finalData, "trafficData":trafficData}
+#         finalData.append(dataObj)
+#         trafficLink.append(lastLink)
+
+#         if dataObj['node_id'] == end['node_id']:
+#             for i in trafficLink:
+#                 sql = f"SELECT * FROM daejeon_traffic where linkID = {i}"
+#                 row = db_class.executeAll(sql)
+#                 if len(row) != 0:
+#                     trafficData.append(row[0])
+#                 else:
+#                     trafficData.append({
+#                         "congestion": "정보없음",
+#                         "endNodeID": "정보없음",
+#                         "endNodeName": "정보없음",
+#                         "linkCount": "정보없음",
+#                         "linkID": "정보없음",
+#                         "linkLength": "정보없음",
+#                         "linkSqc": "정보없음",
+#                         "roadName": "정보없음",
+#                         "speed": "정보없음",
+#                         "startNodeId": "정보없음",
+#                         "startNodeName": "정보없음",
+#                         "travelT": "정보없음",
+#                         "udType": "정보없음"
+#                     })
+
+#             print('end')
+
+#         else:
+#             navi(dataObj, end)
+
+#     navi(nodeData[0], nodeData[1])
+
+#     return {"finalData": finalData, "trafficData": trafficData}
 
 
 if __name__ == '__main__':
